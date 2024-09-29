55189f7689ce8c3a45b369f2e45e6cd83c669a23
humayra_eva <humayra2419@gmail.com>
2024-09-29 20:18:48 +0600

    Fix - removed unnecessary files

--- server/src/validationMessages.ts
+++
@@ -1,9 +0,0 @@
-export const validationMessages = {
-  userAlreadyExists: 'User already exists',
-  verifyEmail: 'Please verify your email',
-  tokenExpired: 'Token has been expired',
-  userVerified: 'User is verified',
-  userNotVerified: 'User not verified',
-  invalidToken: 'Invalid token',
-  userDoesNotExist: 'User does not exist',
-};
